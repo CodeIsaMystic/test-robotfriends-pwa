66d16345ade6f39950b6be2c25e78375fc223cc0
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-15 18:54:39 +0100

    introducing JEST Snapshots, tests thoughts and setting up the package.json with react-test-renderer to render the component using toMatchSnapshot method

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -5,18 +5,43 @@
  * Render: to render to a static HTML 
  *  ==> in between shallow & mount
  */
-import { shallow, mount, render } from 'enzyme';
+//import { shallow } from 'enzyme';
 
 import React from 'react';
+import { create } from "react-test-renderer";
 import Card from './Card';
 
 
 //console.log(shallow(<Card />)); 
 
-
-/* shallow render this Card component  
- * one thing at the time, like unit test
- */
+/* SHALLOW render this Card component  
+ * one thing at the time, like unit test 
 it('to render Card Component', () => {
   expect(shallow(<Card />).length).toEqual(1);
+});
+*/
+
+
+
+/* SNAPSHOT: takes a picture and shows if there are 
+some changes as a test failed
+=> then press w: back on the watch jest mode 
+  => possibility to update if the change is needed
+
+
+
+
+const card = shallow(<Card />);
+
+it('to render Card Component', () => {
+  expect(card).toMatchSnapshot();
+});
+
+
+  Snapshot with react-test-renderer  */
+const card = create(<Card />);
+
+
+it('to render Card Component with react test renderer', () => {
+  expect(card.toJSON()).toMatchSnapshot();
 });
