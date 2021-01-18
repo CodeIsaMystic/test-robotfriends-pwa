b8f03886040ec84791ea7c629bc57b40f1b70e49
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-18 10:45:18 +0100

    improving api tests on actions.test.js

--- src/actions.test.js
+++ src/actions.test.js
@@ -11,9 +11,20 @@ import thunkMiddleware from 'redux-thunk';
 
 
 
+
+let store;
+let mockStore;
+
+beforeEach(() => {
+  mockStore = configureMockStore([thunkMiddleware]);
+  store = mockStore({});
+});
+
+
+
 /* configuring a store to handle dispatch 
 possibility to export and to use on the tests */
-const mockStore = configureMockStore([thunkMiddleware]);
+//const mockStore = configureMockStore([thunkMiddleware]);
 
 describe('setSearchfield actions', () => {
   it('should return an action to search robots', () => {
@@ -27,15 +38,12 @@ describe('setSearchfield actions', () => {
   });
 });
 
-
-
-
-describe('requestRobots pending action', () => {
+/*describe('requestRobots pending action', () => {
   const store = mockStore();
   store.dispatch(actions.requestRobots());
 
   const action = store.getActions();
-  console.log('action', action);
+  //console.log('action', action);
 
   const expectedAction = {
     type: REQUEST_ROBOTS_PENDING
@@ -44,5 +52,41 @@ describe('requestRobots pending action', () => {
   it('handles requesting robots api', () => {
     expect(action[0])
       .toEqual(expectedAction)
+  });
+});*/
+
+
+describe('requestRobotsAction', () => {
+
+  it('should be pending and fail the fetch call', async () => {
+    expect.assertions(2);
+    const wrongApiLink = 'https://jsonplac';
+    await store.dispatch(actions.requestRobots(wrongApiLink));
+
+    const action = store.getActions();
+
+    expect(action[0].type).toEqual(REQUEST_ROBOTS_PENDING);
+    expect(action[1].type).toEqual(REQUEST_ROBOTS_FAILED);
+
+  });
+
+
+  it('should be pending and success', async () => {
+    expect.assertions(3);
+
+    /* Testing the API  */
+    const testingAPIurl = 'https://jsonplaceholder.typicode.com/todos/1';
+    const action = store.getActions();
+
+    await store.dispatch(actions.requestRobots(testingAPIurl));
+
+    expect(action[0].type).toEqual(REQUEST_ROBOTS_PENDING);
+    expect(action[1].type).toEqual(REQUEST_ROBOTS_SUCCESS);
+    expect(action[1].payload).toEqual({
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false
+    });
   });
 });
